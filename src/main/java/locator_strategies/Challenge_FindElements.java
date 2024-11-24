f2b646061f3e3e57f3a2fca80e1324e22837a7a6
Kinga <37161970+madking55@users.noreply.github.com>
2024-11-24 17:30:08 +0000

    add code challende

---
+++ src/main/java/locator_strategies/Challenge_FindElements.java
@@ -0,0 +1,21 @@
+package locator_strategies;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import setup.Main;
+
+public class Challenge_FindElements extends Main {
+  @SuppressWarnings("unused")
+
+  public static void main(String[] args) {
+    driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+
+    WebElement imageButton = driver.findElement(By.id("imageButton"));
+    WebElement clickButton = driver.findElement(By.cssSelector("input[value='Click!']"));
+    WebElement nameTextBox = driver.findElement(By.name("x"));
+
+    driver.quit();
+  }
+
+}
